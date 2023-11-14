41468ec3bb576075137676f4094c515bb8f89ec4
Mozart Rueckert <mozartrue@outlook.com>
2023-11-13 22:31:11 -0300

    feat: create in memory user repository

---
+++ src/repositories/in-memory/in-memory-user-repository.ts
@@ -0,0 +1,23 @@
+import { UserModel } from "../../models/user-model";
+import { UserRepository } from "../user-repository";
+
+export class InMemoryUserRepository implements UserRepository {
+  public items: UserModel[] = [];
+
+  async create(data: UserModel) {
+    this.items.push(data);
+    return data;
+  }
+
+  async findByEmail(email: string) {
+    const user = this.items.find((item) => {
+      return item.email === email;
+    });
+
+    if (!user) {
+      return null;
+    }
+
+    return user;
+  }
+}
