fdf4a4ab416fa76907d3602db7db03cf38fbd955
Mozart Rueckert <mozartrue@outlook.com>
2023-11-13 22:30:50 -0300

    feat: set interface of user repository

---
+++ src/repositories/user-repository.ts
@@ -0,0 +1,6 @@
+import { UserModel } from "../models/user-model";
+
+export interface UserRepository {
+  create(data: UserModel): Promise<UserModel>;
+  findByEmail(email: string): Promise<UserModel | null>;
+}
