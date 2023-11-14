c1544e92921bfdef4da7d3ea7ac22df66213d89d
Mozart Rueckert <mozartrue@outlook.com>
2023-11-13 22:52:24 -0300

    feat: set test to validate if password is hashed

--- src/use-cases/register/register.spec.ts
+++ src/use-cases/register/register.spec.ts
@@ -1,6 +1,7 @@
 import { beforeEach, describe, expect, it } from "vitest";
 import { RegisterUseCase } from "./register";
 import { InMemoryUserRepository } from "../../repositories/in-memory/in-memory-user-repository";
+import { compare } from "bcryptjs";
 
 let userRepository: InMemoryUserRepository;
 let sut: RegisterUseCase;
@@ -53,5 +54,23 @@ describe("Register Use Case", () => {
         cep: "89000-000",
       }),
     ).rejects.toBeInstanceOf(Error);
+  });
+
+  it("should encrypt the user password on registration", async () => {
+    const { user } = await sut.exec({
+      id: "userID-01",
+      name: "John Doe",
+      email: "example@email.com",
+      password: "keyword123",
+      phoneNumber: "00 5555-5555",
+      estate: "Santa Catarina",
+      city: "Blumenau",
+      neightborhood: "Vila Nova",
+      cep: "89000-000",
+    });
+
+    const isUserPasswordEncrypted = await compare("keyword123", user.password);
+
+    expect(isUserPasswordEncrypted).toBe(true);
   });
 });
