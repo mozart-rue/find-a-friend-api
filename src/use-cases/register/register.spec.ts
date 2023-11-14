f1d45875d2f3cd4a64a81b3c4410b6adf79c7461
Mozart Rueckert <mozartrue@outlook.com>
2023-11-13 22:32:35 -0300

    feat: set test to create an user

---
+++ src/use-cases/register/register.spec.ts
@@ -0,0 +1,29 @@
+import { beforeEach, describe, expect, it } from "vitest";
+import { RegisterUseCase } from "./register";
+import { InMemoryUserRepository } from "../../repositories/in-memory/in-memory-user-repository";
+
+let userRepository: InMemoryUserRepository;
+let sut: RegisterUseCase;
+
+describe("Register Use Case", () => {
+  beforeEach(() => {
+    userRepository = new InMemoryUserRepository();
+    sut = new RegisterUseCase(userRepository);
+  });
+
+  it("should be able to register", async () => {
+    const { user } = await sut.exec({
+      id: "userID-01",
+      name: "John Doe",
+      email: "example@email.com",
+      password: "keyword123",
+      phoneNumber: "00 5555-5555",
+      estate: "Santa Catarina",
+      city: "Blumenau",
+      neightborhood: "Vila Nova",
+      cep: "89000-000",
+    });
+
+    expect(user.id).toEqual("userID-01");
+  });
+});
