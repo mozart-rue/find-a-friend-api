6602cf5233e7daea0b2d402fa61ab3eec53e0f97
Mozart Rueckert <mozartrue@outlook.com>
2023-11-16 20:40:33 -0300

    feat: set test to validate if return a complete detailed pet

---
+++ src/use-cases/pets/get-pet.spec.ts
@@ -0,0 +1,58 @@
+import { beforeEach, describe, expect, it } from "vitest";
+import { InMemoryPetRepository } from "../../repositories/in-memory/in-memory-pet-repository";
+import { GetPetUseCase } from "./get-pet";
+import { InMemoryUserRepository } from "../../repositories/in-memory/in-memory-user-repository";
+import { UserModel } from "../../models/user-model";
+import { hash } from "bcryptjs";
+import { PetModel } from "../../models/pet-model";
+
+let userRepository: InMemoryUserRepository;
+let petRepository: InMemoryPetRepository;
+let sut: GetPetUseCase;
+
+describe("Get Pet Use Case", () => {
+  beforeEach(() => {
+    userRepository = new InMemoryUserRepository();
+    petRepository = new InMemoryPetRepository();
+    sut = new GetPetUseCase(petRepository);
+  });
+
+  it("should get a pet details", async () => {
+    const org = new UserModel({
+      name: "John Doe",
+      email: "example@email.com",
+      password: await hash("keyword123", 6),
+      phoneNumber: "00 5555-5555",
+      estate: "Santa Catarina",
+      city: "Blumenau",
+      neightborhood: "Vila Nova",
+      cep: "89000-000",
+    });
+
+    const user = await userRepository.create(org);
+
+    const petModel = new PetModel({
+      org: user,
+      name: `Cachorro`,
+      age: 4,
+      type: "Cachorro",
+      size: "MEDIUM",
+      energy: 5,
+      description: "Cãozinho muito amigavel e legal",
+      adoptionRequirements: [],
+      photos: [],
+    });
+
+    const newPet = await petRepository.create(petModel);
+
+    const { pet } = await sut.exec({ petId: newPet.id });
+
+    expect(pet.id).toEqual(expect.any(String));
+    expect(pet).toEqual(
+      expect.objectContaining({
+        name: "Cachorro",
+        org: expect.objectContaining({ city: "Blumenau" }),
+      }),
+    );
+  });
+});
