629eaaf5ec8b996255eae6df8ab29bc91ebb2ee5
Mozart Rueckert <mozartrue@outlook.com>
2023-11-15 23:03:08 -0300

    feat: set optional params to filter

--- src/repositories/pet-repository.ts
+++ src/repositories/pet-repository.ts
@@ -3,9 +3,20 @@ import { PetModel } from "../models/pet-model";
 export interface FetchRequest {
   estate: string;
   city: string;
+  age?: number;
+  type?: string;
+  size?: string;
+  energy?: number;
 }
 
 export interface PetRepository {
   create(pet: PetModel): Promise<PetModel>;
-  fetchByCityAndEstate({ estate, city }: FetchRequest): Promise<PetModel[]>;
+  fetchByCityAndEstate({
+    estate,
+    city,
+    age,
+    type,
+    size,
+    energy,
+  }: FetchRequest): Promise<PetModel[]>;
 }
