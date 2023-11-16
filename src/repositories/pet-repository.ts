7e7d89b2bdbfa622577ed3371d1e5c25ee572379
Mozart Rueckert <mozartrue@outlook.com>
2023-11-15 21:36:16 -0300

    feat: set method fetchByCityAndEstate

--- src/repositories/pet-repository.ts
+++ src/repositories/pet-repository.ts
@@ -1,5 +1,11 @@
 import { PetModel } from "../models/pet-model";
 
+export interface FetchRequest {
+  estate: string;
+  city: string;
+}
+
 export interface PetRepository {
   create(pet: PetModel): Promise<PetModel>;
+  fetchByCityAndEstate({ estate, city }: FetchRequest): Promise<PetModel[]>;
 }
