7014f913b4ea5d050b2feed2cbb09df506359a99
Mozart Rueckert <mozartrue@outlook.com>
2023-11-15 23:04:47 -0300

    feat: add optional params to filter pet list

--- src/use-cases/pets/fetch-pets.ts
+++ src/use-cases/pets/fetch-pets.ts
@@ -6,6 +6,10 @@ import { EstateNotFilterError } from "../errors/estate-not-filter-error";
 interface FetchPetsUseCaseRequest {
   estate: string;
   city: string;
+  age?: number;
+  type?: string;
+  size?: string;
+  energy?: number;
 }
 
 interface FetchPetsUseCaseResponse {
@@ -18,6 +22,10 @@ export class FetchPetsUseCase {
   async exec({
     estate,
     city,
+    age,
+    type,
+    size,
+    energy,
   }: FetchPetsUseCaseRequest): Promise<FetchPetsUseCaseResponse> {
     if (estate.trim() === "") {
       throw new EstateNotFilterError();
@@ -30,6 +38,10 @@ export class FetchPetsUseCase {
     const pets = await this.petRepository.fetchByCityAndEstate({
       estate,
       city,
+      age,
+      type,
+      size,
+      energy,
     });
     return { pets };
   }
