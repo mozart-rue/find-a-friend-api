498629861b9d46df69471bf229655baf9b6ac5f8
Mozart Rueckert <mozartrue@outlook.com>
2023-11-16 20:47:38 -0300

    feat: validate to not retrieve a pet with wrong id

--- src/use-cases/pets/get-pet.spec.ts
+++ src/use-cases/pets/get-pet.spec.ts
@@ -5,6 +5,7 @@ import { InMemoryUserRepository } from "../../repositories/in-memory/in-memory-u
 import { UserModel } from "../../models/user-model";
 import { hash } from "bcryptjs";
 import { PetModel } from "../../models/pet-model";
+import { ResourceNotFoundError } from "../errors/resource-not-found-error";
 
 let userRepository: InMemoryUserRepository;
 let petRepository: InMemoryPetRepository;
@@ -54,5 +55,38 @@ describe("Get Pet Use Case", () => {
         org: expect.objectContaining({ city: "Blumenau" }),
       }),
     );
+  });
+
+  it("should not be able to get a pet details with wrong id", async () => {
+    const org = new UserModel({
+      name: "John Doe",
+      email: "example@email.com",
+      password: await hash("keyword123", 6),
+      phoneNumber: "00 5555-5555",
+      estate: "Santa Catarina",
+      city: "Blumenau",
+      neightborhood: "Vila Nova",
+      cep: "89000-000",
+    });
+
+    const user = await userRepository.create(org);
+
+    const petModel = new PetModel({
+      org: user,
+      name: `Cachorro`,
+      age: 4,
+      type: "Cachorro",
+      size: "MEDIUM",
+      energy: 5,
+      description: "Cãozinho muito amigavel e legal",
+      adoptionRequirements: [],
+      photos: [],
+    });
+
+    const newPet = await petRepository.create(petModel);
+
+    await expect(() =>
+      sut.exec({ petId: "non-existent-id" }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError);
   });
 });
