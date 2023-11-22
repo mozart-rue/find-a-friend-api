c6fc20fca9c6f0b385fcd0ba7e80a62de1a510f7
Mozart Rueckert <mozartrue@outlook.com>
2023-11-21 22:50:23 -0300

    feat: set route to authentication

--- src/http/controllers/user/route.ts
+++ src/http/controllers/user/route.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from "fastify";
 import { register } from "./register";
+import { authenticate } from "./authenticate";
 
 export async function userRoutes(app: FastifyInstance) {
   app.post("/user", register);
+  app.post("/sessions", authenticate);
 }
